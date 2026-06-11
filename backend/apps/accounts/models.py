c8776e4adcb6f1dcdc91c8d988b94627ccfe9bde
prince220504 <panchalprince2512@gmail.com>
2026-06-11 23:07:22 +0530

    feat(accounts): add CustomUser model with role, email, phone fields

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        DOCTOR = 'DOCTOR', 'Doctor'
+        RECEPTION = 'RECEPTION', 'Receptionist'
+        LAB = 'LAB', 'Lab Technician'
+        PATIENT = 'PATIENT','Patient'
+
+    phone_validator = RegexValidator(regex=r'^[6-9]\d{9}$', message='Enter a valid 10-digit Indian mobile number (must start with 6, 7, 8, or 9).',)
+
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10,validators=[phone_validator], blank=True,)
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.PATIENT,
+    )
+
+    @property
+    def phone_with_code(self):
+        return f"+91{self.phone}" if self.phone else ''
+
+    def __str__(self):
+        return f"{self.username}({self.get_role_display()})"
