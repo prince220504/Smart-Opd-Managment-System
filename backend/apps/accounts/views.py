7a9d459106ba5eee384d188b254e16128ea548d5
prince220504 <panchalprince2512@gmail.com>
2026-06-15 22:45:25 +0530

    feat(accounts): add placeholder login/logour/register views

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def login_view(request):
+    return HttpResponse('Login page - coming in Step 12')
+
+def logout_view(request):
+    return HttpResponse('Logout - coming in Step 12')
+
+def register_view(request):
+    return HttpResponse('Register page - coming in Step 12')
