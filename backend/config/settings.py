b657616785983162ac3c731b06f2de4ddb027426
prince220504 <panchalprince2512@gmail.com>
2026-08-16 19:46:55 +0530

    fix(config): correct sqlite fallback path typo

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(
-        config('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqllite3'),
+        config('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqlite3'),
         conn_max_age=600,
     )
 }
